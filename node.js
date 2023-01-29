1e9e27dcea4ba7aedd118464464eac3a1110cd55
AnshikaBangar <69668387+AnshikaBangar@users.noreply.github.com>
2023-01-29 12:20:46 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 // Save product to local storage
-localStorage.clear();
+//localStorage.clear();
 document.getElementById("save-product").addEventListener("click", function(){
     var productId = document.getElementById("product-id").value;
     var sellingPrice = document.getElementById("selling-price").value;
@@ -22,7 +22,44 @@ document.getElementById("save-product").addEventListener("click", function(){
     localStorage.removeItem(productId);
     displayProducts();
   }
-  
+  // Edit product in local storage
+function editProduct(productId){
+    var product = JSON.parse(localStorage.getItem(productId));
+    document.getElementById("product-id").value = product.id;
+    document.getElementById("selling-price").value = product.price;
+    document.getElementById("product-name").value = product.name;
+    document.getElementById("category").value = product.category;
+    document.getElementById("save-product").innerHTML = "Save";
+    document.getElementById("save-product").removeEventListener("click", function(){
+    var productId = document.getElementById("product-id").value;
+    var sellingPrice = document.getElementById("selling-price").value;
+    var productName = document.getElementById("product-name").value;
+    var category= document.getElementById("category").value;
+    var product = {
+      id: productId,
+      price: sellingPrice,
+      name: productName,
+      category: category
+    }
+    localStorage.setItem(productId, JSON.stringify(product));
+    displayProducts();
+    });
+    document.getElementById("save-product").addEventListener("click", function(){
+    var productId = document.getElementById("product-id").value;
+    var sellingPrice = document.getElementById("selling-price").value;
+    var productName = document.getElementById("product-name").value;
+    var category= document.getElementById("category").value;
+    var product = {
+      id: productId,
+      price: sellingPrice,
+      name: productName,
+      category: category
+    }
+    localStorage.setItem(productId, JSON.stringify(product));
+    displayProducts();
+    });
+}
+
   // Display stored products
   function displayProducts() {
     var productList = document.getElementById("product-list");
@@ -57,7 +94,14 @@ document.getElementById("save-product").addEventListener("click", function(){
         deleteProduct(product.id);
       });
       productDiv.appendChild(deleteBtn);
-  
+            // Create "edit" button
+      var editBtn = document.createElement("button");
+      editBtn.innerHTML = "Edit";
+      editBtn.addEventListener("click", function(){
+        editProduct(product.id);
+      });
+      productDiv.appendChild(editBtn);
+
       productList.appendChild(productDiv);
     }
   }
